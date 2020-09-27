9e0c0d544f969fb0b9efae8f2898c0e29b35c3dd
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-09-27 09:13:49 +0900

    add global stype(reset css, font, color variables

--- src/App.vue
+++ src/App.vue
@@ -2,3 +2,13 @@
   <div id="nav"><router-link to="/">Home</router-link> |</div>
   <router-view />
 </template>
+
+<style lang="scss">
+@import url("https://fonts.googleapis.com/css2?family=Raleway:wght@500;600;700&display=swap");
+@import "@/assets/variables.scss";
+
+#app {
+  font-family: "Raleway", sans-serif;
+  color: $color-text;
+}
+</style>
