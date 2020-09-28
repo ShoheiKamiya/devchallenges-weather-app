2e354614a405f04bae5dfc86242cbaf7bd0861b4
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-09-28 20:49:59 +0900

    add base style(nav, main)

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,19 @@
 <template>
-  <router-view />
+  <div class="root">
+    <nav class="nav">nav</nav>
+    <Main class="main" />
+  </div>
 </template>
+
+<script lang="ts">
+import Main from "@/views/Main.vue";
+
+export default {
+  components: {
+    Main
+  }
+};
+</script>
 
 <style lang="scss">
 @import url("https://fonts.googleapis.com/css2?family=Raleway:wght@500;600;700&display=swap");
@@ -9,6 +22,20 @@
 #app {
   font-family: "Raleway", sans-serif;
   color: $color-text;
+  background: $color-background;
   font-size: 16px;
+}
+
+.root {
+  display: flex;
+
+  .nav {
+    width: 459px;
+    background: $color-base;
+  }
+
+  .main {
+    padding: 42px 123px 50px 154px;
+  }
 }
 </style>
