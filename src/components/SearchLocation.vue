d6cd2d75fae7412935d95d588c65db60b650af2a
ShoheiKamiya <shoooooo.bqt@gmail.com>
2020-10-06 23:24:25 +0900

    add loading at location search

--- src/components/SearchLocation.vue
+++ src/components/SearchLocation.vue
@@ -1,11 +1,14 @@
 <template>
-  <input
-    class="search-location"
-    type="text"
-    placeholder="search location"
-    @input="search"
-    ref="locationInput"
-  />
+  <div class="search">
+    <input
+      class="input"
+      type="text"
+      placeholder="search location"
+      @input="search"
+      ref="locationInput"
+    />
+    <BaseLoading class="loading" v-if="isSearching" />
+  </div>
   <ul>
     <li v-for="city in suggestedCities" :key="city.woeid">{{ city.title }}</li>
   </ul>
@@ -16,25 +19,37 @@ import { defineComponent, ComponentPublicInstance, PropType } from "vue";
 import { fetchLocation } from "@/api/metaWeather";
 import { debounce } from "throttle-debounce";
 import { Location } from "../../models/Location";
+import BaseLoading from "@/components/BaseLoading.vue";
 
 export default defineComponent({
+  components: {
+    BaseLoading
+  },
   data() {
     return {
-      suggestedCities: [] as PropType<Location[]>
+      suggestedCities: [] as PropType<Location[]>,
+      isSearching: false
     };
   },
   methods: {
-    search: debounce(1000, function(
-      this: ComponentPublicInstance<{}, {}, { suggestedCities: Location[] }>
+    search: debounce(200, function(
+      this: ComponentPublicInstance<
+        {},
+        {},
+        { suggestedCities: Location[]; isSearching: boolean }
+      >
     ): void {
       const locationInput = this.$refs.locationInput as HTMLInputElement;
       const city = locationInput.value;
       if (city === "") {
         this.suggestedCities = [];
+        this.isSearching = false;
         return;
       }
+      this.isSearching = true;
       fetchLocation(city).then(res => {
         this.suggestedCities = res.data;
+        this.isSearching = false;
       });
     })
   }
@@ -42,10 +57,21 @@ export default defineComponent({
 </script>
 
 <style lang="scss" scoped>
-.search-location {
+.search {
+  position: relative;
   width: 268px;
-  height: 47px;
-  padding: 15px;
-  border: 1px solid #e7e7eb;
+
+  .input {
+    width: 100%;
+    height: 47px;
+    padding: 15px;
+    border: 1px solid #e7e7eb;
+  }
+
+  .loading {
+    position: absolute;
+    top: 13px;
+    right: 13px;
+  }
 }
 </style>
